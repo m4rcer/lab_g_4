3999905b8f8f4d51a9e82e576e05637a00f21a27
m4rcer <m4rcer@gmail.com>
2022-11-30 00:39:52 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace lab_g_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.orderInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.drawBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(1432, 807);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // orderInput
+            // 
+            this.orderInput.Location = new System.Drawing.Point(179, 761);
+            this.orderInput.Name = "orderInput";
+            this.orderInput.Size = new System.Drawing.Size(100, 26);
+            this.orderInput.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 761);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Порядок кривой";
+            // 
+            // drawBtn
+            // 
+            this.drawBtn.Location = new System.Drawing.Point(303, 755);
+            this.drawBtn.Name = "drawBtn";
+            this.drawBtn.Size = new System.Drawing.Size(148, 38);
+            this.drawBtn.TabIndex = 3;
+            this.drawBtn.Text = "Нарисовать";
+            this.drawBtn.UseVisualStyleBackColor = true;
+            this.drawBtn.Click += new System.EventHandler(this.drawBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1432, 807);
+            this.Controls.Add(this.drawBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.orderInput);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.TextBox orderInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button drawBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+
+namespace lab_g_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string getNthOrderDragonCurve(int n)
+        {
+            if (n <= 0)
+                return "";
+
+            var builder = new StringBuilder("1");
+
+            for (int i = 1; i < n; i++)
+            {
+                builder.Append("1");
+                var prevOrder = new StringBuilder(builder.ToString());
+                prevOrder.Remove(prevOrder.Length - 1, 1);
+                prevOrder[
+                    (int)Math.Ceiling(
+                        ((double)prevOrder.Length / 2) - 1)
+                    ] = '0';
+                builder.Append(prevOrder);
+            }
+
+            return builder.ToString();
+        }
+
+        private void drawBtn_Click(object sender, EventArgs e)
+        {
+            var order = 5;
+            if(orderInput.Text != "")
+                order = int.Parse(orderInput.Text);
+
+            var pen = new Pen(Color.Black, 6);
+            pen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+            var tailPen = new Pen(Color.Black, 6);
+            tailPen.CompoundArray = new float[] { 0.0f, 0.1f, 0.5f, 1.0f };
+            var g = pictureBox.CreateGraphics();
+
+
+            int dx = 40;
+
+            string str = getNthOrderDragonCurve(order);
+
+            int x1 = pictureBox.Size.Width / 2;
+            int y1 = pictureBox.Size.Height / 2;
+
+            int x2 = pictureBox.Size.Width / 2;
+            int y2 = pictureBox.Size.Height / 2 - dx;
+
+            int x3 = x2; int y3 = y2;
+
+            g.DrawLine(pen, x1, y1, x3, y3);
+
+            for (int i = 0; i < str.Length; i++)
+            {
+
+                if (y2 - y1 < 0)
+                { 
+                    if (str[i] == '1') x3 = x2 - dx; 
+                    else x3 = x2 + dx; 
+                    y3 = y2;
+                }
+                if (x2 - x1 < 0)
+                { 
+                    if (str[i] == '1') y3 = y2 - dx; 
+                    else y3 = y2 + dx; 
+                    x3 = x2;
+                }
+                if (x2 - x1 > 0)
+                { 
+                    if (str[i] == '1') y3 = y2 + dx; 
+                    else y3 = y2 - dx; 
+                    x3 = x2;
+                }
+                if (y2 - y1 > 0)
+                { 
+                    if (str[i] == '1') x3 = x2 + dx; 
+                    else x3 = x2 - dx;
+                    y3 = y2;
+                }
+                if (i == str.Length - 1)
+                {
+                    g.DrawLine(tailPen, x2, y2, x3, y3);
+                }
+                else
+                { 
+                    g.DrawLine(pen, x2, y2, x3, y3);
+                }
+                
+                x1 = x2; y1 = y2;
+                x2 = x3; y2 = y3;
+
+            }
+
+        pen.Dispose();
+            g.Dispose();
+        }
+    }
+}
